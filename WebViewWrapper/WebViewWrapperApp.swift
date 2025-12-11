99ff3507520e821fadbbff13a83ba5808b80b99e
Sabbir Hossain <sabbirsopnil07@gmail.com>
2025-12-11 15:26:23 +0600

    Initial Commit

---
+++ WebViewWrapper/WebViewWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewWrapperApp.swift
+//  WebViewWrapper
+//
+//  Created by Sopnil Sohan on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct WebViewWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
